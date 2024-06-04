5870acba5e994f8a49b0e139b2d99bb61fb40232
Aayush Gelal <aayushgelal@gmail.com>
2024-06-04 18:35:28 +0545

    fixed deployments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
 import { redirect } from 'next/navigation';
-import { NextApiRequest, NextApiResponse } from 'next';
 
 import React from 'react'
 
-export default function page(req:NextApiRequest, res:NextApiResponse) {
+export default function page() {
   return redirect('/home');
 }
