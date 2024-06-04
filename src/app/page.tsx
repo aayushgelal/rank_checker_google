064ab29e392f105ca92c4ebb587d7902f47651d8
Aayush Gelal <aayushgelal@gmail.com>
2024-06-04 17:43:12 +0545

    fixed redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
-import { redirect } from 'next/dist/server/api-utils'
+import { redirect } from 'next/navigation';
+import { NextApiRequest, NextApiResponse } from 'next';
+
 import React from 'react'
 
-export default function page() {
-    
-
-  return (
-    <div>page</div>
-  )
+export default function page(req:NextApiRequest, res:NextApiResponse) {
+  return redirect('/home');
 }
